11910d5ea22af298e391f4499759ad941a1f6d20
pigranya1218 <ddodd34@gmail.com>
2020-08-10 09:22:40 +0900

    StageType 이넘 클래스 생성
    현재 스테이지의 타입이 무엇인지 판별할 수 있도록 생성

--- Dungreed/StageManager.h
+++ Dungreed/StageManager.h
@@ -1,6 +1,18 @@
 #pragma once
+
+enum class STAGE_TYPE : int
+{
+	VILLAGE = 0,
+	DUNGEON_NORMAL,
+	DUNGEON_BOSS
+};
+
 class StageManager
 {
+private:
+	STAGE_TYPE _currStageType;
+
+public:
 
 };
 
