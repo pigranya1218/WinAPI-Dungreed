65553aad3cb8ff0927e29c84fcb655a037bf046a
milrow321 <59471824+milrow321@users.noreply.github.com>
2020-08-11 09:43:03 +0900

    임시커밋

--- Dungreed/testStage.cpp
+++ Dungreed/testStage.cpp
@@ -3,9 +3,16 @@
 void TestStage::init()
 {
 	Stage::init();
-	_collisions.push_back({LinearFunc::getLinearFuncFromPoints(Vector2(0, 800), Vector2(WINSIZEX, 800)), LINEAR_VALUE_TYPE::DOWN});
-	_collisions.push_back({LinearFunc::getLinearFuncFromPoints(Vector2(50, 100), Vector2(50, 800)), LINEAR_VALUE_TYPE::LEFT});
-	_collisions.push_back({LinearFunc::getLinearFuncFromPoints(Vector2(1550, 100), Vector2(1550, 800)), LINEAR_VALUE_TYPE::RIGHT});
+	_collisions.push_back({ LinearFunc::getLinearFuncFromPoints(Vector2(0, 800), Vector2(WINSIZEX, 800)), LINEAR_VALUE_TYPE::DOWN });
+	_collisions.push_back({ LinearFunc::getLinearFuncFromPoints(Vector2(50, 100), Vector2(50, 800)), LINEAR_VALUE_TYPE::LEFT });
+	_collisions.push_back({ LinearFunc::getLinearFuncFromPoints(Vector2(1550, 100), Vector2(1550, 800)), LINEAR_VALUE_TYPE::RIGHT });
+
+	_tileImage = IMAGE_MANAGER->findImage("sampleTile");
+	mapLoad();
+	for (int i = 0; i < TILEX*TILEY; ++i)
+	{
+		_collisions.push_back({ LinearFunc::getLinearFuncFromPoints(Vector2(_tile[i].rc.left,_tile[i].rc.top),Vector2(_tile[i].rc.right,_tile[i].rc.top)),LINEAR_VALUE_TYPE::DOWN });
+	}
 }
 
 void TestStage::release()
@@ -20,6 +27,11 @@ void TestStage::update(float const elapsedTime)
 
 void TestStage::render()
 {
+	for (int i = 0; i < TILEX*TILEY; ++i)
+	{
+		_tileImage->frameRender(_tile[i].rc.getCenter(), _tile[i].tileFrameX, _tile[i].tileFrameY);
+	}
+
 	for (int i = 0; i < _collisions.size(); i++)
 	{
 		D2D_RENDERER->drawLine(_collisions[i].func.getStart(), _collisions[i].func.getEnd());
