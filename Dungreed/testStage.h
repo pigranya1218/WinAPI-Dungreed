f34509187cf1c44da944cb019d8861c3df5f0723
milrow321 <59471824+milrow321@users.noreply.github.com>
2020-08-11 09:27:55 +0900

    임시커밋

--- Dungreed/testStage.h
+++ Dungreed/testStage.h
@@ -1,14 +1,19 @@
 #pragma once
 #include "Stage.h"
+#include "MapTool.h"
 
 class TestStage : public Stage
 {
 private:
+
+	tagTileMap _tile;
 
 public:
 	virtual void init();
 	virtual void release();
 	virtual void update(float const elapsedTime);
 	virtual void render();
+
+	virtual void mapLoad();
 };
 
