6ddba198caa1f4c2a8b45f304f1df28324515737
zpfhfh0124 <48612497+zpfhfh0124@users.noreply.github.com>
2020-08-11 10:58:17 +0900

    디폴트 스탯 지정

--- Dungreed/Player.cpp
+++ Dungreed/Player.cpp
@@ -4,10 +4,22 @@
 
 void Player::setBaseStat()
 {
-	_baseStat.maxHp = 75;
+	_baseStat.maxHp = 80;
 	_baseStat.maxJumpCount = 1;
 	_baseStat.maxDashCount = 2;
 	_baseStat.maxSatiety = 100;
+	_baseStat.power = 5;
+	_baseStat.damage = 0; //0 ~ 4
+	_baseStat.trueDamage = 0;
+	_baseStat.criticalChance = 2;
+	_baseStat.criticalDamage = 1; // + 100%
+	_baseStat.toughness = 0;
+	_baseStat.defense = 0;
+	_baseStat.block = 0;
+	_baseStat.evasion = 0;
+	_baseStat.attackSpeed = 0;
+	_baseStat.reloadSpeed = 0; // --
+	_baseStat.moveSpeed = 350;
 	_baseStat.dashXPower = 2400;
 	_baseStat.dashYPower = 1700;
 	_baseStat.jumpPower = 1700;
@@ -161,7 +173,7 @@ void Player::update(float const elapsedTime)
 	//좌측 이동중
 	if (KEY_MANAGER->isStayKeyDown(CONFIG_MANAGER->getKey(ACTION_TYPE::MOVE_LEFT)))
 	{
-		moveDir.x -= 350 * elapsedTime;
+		moveDir.x -= _baseStat.moveSpeed * elapsedTime;
 		if (_isStand)
 		{
 			if (moveDir.x == 0)
@@ -179,7 +191,7 @@ void Player::update(float const elapsedTime)
 	//우측 이동중
 	if (KEY_MANAGER->isStayKeyDown(CONFIG_MANAGER->getKey(ACTION_TYPE::MOVE_RIGHT)))
 	{
-		moveDir.x += 350 * elapsedTime;
+		moveDir.x += _baseStat.moveSpeed * elapsedTime;
 
 		if (_isStand)
 		{
