92113c6b21863e76e9376fc08cfd54ac25cd9e87
RhinoMrMoon <68267826+RhinoMrMoon@users.noreply.github.com>
2020-08-17 09:16:52 +0900

    활 완벽
    활 연성 성공적

--- Dungreed/OakBow.cpp
+++ Dungreed/OakBow.cpp
@@ -128,7 +128,7 @@ void OakBow::update(Player * player, float const elapsedTime)
 			projectile->setPosition(arrowPos);
 			projectile->setSize(Vector2(80, 15));
 			projectile->setTeam(OBJECT_TEAM::PLAYER);
-			projectile->init("Arrow00", angleRadian, 0, false, false, 20, false, "", Vector2());
+			projectile->init("Arrow00", angleRadian, 30 * 50, false, false, 20, false, "", Vector2());
 
 			AttackInfo* attackInfo = new AttackInfo;
 			attackInfo->team = OBJECT_TEAM::PLAYER;
