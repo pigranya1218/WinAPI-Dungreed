a296a5bf54c2a3e6127459d54126d978dcdc48eb
pigranya1218 <ddodd34@gmail.com>
2020-08-18 17:31:58 +0900

    주먹 공격판정 구현

--- Dungreed/Punch.cpp
+++ Dungreed/Punch.cpp
@@ -40,9 +40,14 @@ void Punch::update(Player* player, float const elapsedTime)
 		_attackMove.x = min(abs(cosf(_attackAngle) * 30), _attackMove.x + abs(cosf(_attackAngle) * 30 * ratio));
 		_attackMove.y = min(abs(-sinf(_attackAngle) * 30), _attackMove.y + abs((-sinf(_attackAngle)) * 30 * ratio));
 
+		// 찌르는 모션동안 계속 공격판정
+		player->attack(_attackCircle, _attackInfo);
+
 		if (_currAttackDelay <= _adjustStat.attackSpeed * 0.88)
 		{
 			_reverseMove = true;
+			delete _attackCircle;
+			delete _attackInfo;
 		}
 	}
 
@@ -83,6 +88,11 @@ void Punch::backRender(Player* player)
 		_rightHand = rectMakePivot(renderPosRight, Vector2(_handSize), PIVOT::CENTER);
 		D2D_RENDERER->fillRectangle(CAMERA->getRelativeFR(_rightHand), 210, 188, 181, 1.f, angle, CAMERA->getRelativeV2(_rightHand.getCenter()));
 		D2D_RENDERER->drawRectangle(CAMERA->getRelativeFR(_rightHand), 40, 36, 58, 1, 2.f, angle, CAMERA->getRelativeV2(_rightHand.getCenter()));
+	}
+
+	if (_attackCircle != nullptr)
+	{
+		_attackCircle->render(true);
 	}
 }
 
@@ -132,6 +142,31 @@ void Punch::attack(Player* player)
 	_reverseMove = false;
 	_attackAngle = angle;
 	_currAttackDelay = _adjustStat.attackSpeed;
+
+	Vector2 originPos = player->getPosition();
+	originPos.x += ((isLeft) ? (-20) : (20));
+	originPos.y += 20; // 플레이어의 중점으로부터 무기를 들고 있는 높이
+	float attackRadian = atan2f(-(CAMERA->getAbsoluteY(_ptMouse.y) - originPos.y), (CAMERA->getAbsoluteX(_ptMouse.x) - originPos.x));
+	if (attackRadian < 0)
+	{
+		attackRadian += PI2;
+	}
+	string attackCode = to_string(_itemCode) + to_string(TIME_MANAGER->getWorldTime()); // 아이템 코드와 현재 시간을 Concat하여 공격 아이디를 구하기 위한 공격 코드를 생성함
+
+	_attackCircle = new FloatCircle;
+	_attackCircle->origin = originPos;
+	_attackCircle->size = 35;
+	_attackCircle->startRadian = attackRadian - PI * 0.03;
+	_attackCircle->endRadian = attackRadian + PI * 0.03;
+
+	_attackInfo = new AttackInfo;
+	_attackInfo->team = OBJECT_TEAM::PLAYER;
+	_attackInfo->attackID = TTYONE_UTIL::getHash(attackCode);
+	_attackInfo->critical = 0;
+	_attackInfo->criticalDamage = 0;
+	_attackInfo->minDamage = _addStat.minDamage;
+	_attackInfo->maxDamage = _addStat.maxDamage;
+	_attackInfo->knockBack = 5;
 }
 
 void Punch::attack(FloatRect * rect, AttackInfo * info)
