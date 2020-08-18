93d9421a7f8b580c2d2a42ef5712cc9afb86f08a
RhinoMrMoon <68267826+RhinoMrMoon@users.noreply.github.com>
2020-08-18 17:08:30 +0900

    개틀링 연성 중
    개틀링 연성 중

--- Dungreed/GatlingGun.cpp
+++ Dungreed/GatlingGun.cpp
@@ -158,10 +158,17 @@ void GatlingGun::frontRender(Player * player)
 	if (_shootEffectAni->isPlay())
 	{
 		Vector2 shootEffectPos = renderPosHand;
+		
+		//shootEffectPos.x += renderPosHand.x + 70;
+		//shootEffectPos.y = renderPosHand.y + 25;
 
+		float length = _iconImg->getWidth() * 1.1f * 4; // 무기 길이만큼
+		shootEffectPos.x += cosf(degree * (PI / 180) + ((isLeft) ? (-0.2) : (0.2))) * length;
+		shootEffectPos.y += -sinf(degree * (PI / 180) + ((isLeft) ? (-0.2) : (0.2))) * length;
+		
 		_shootEffectImg->setAngle(renderDegree);
 		_shootEffectImg->setScale(4);
-		_shootEffectImg->aniRender(CAMERA->getRelativeV2(renderPosWeapon), _shootEffectAni);
+		_shootEffectImg->aniRender(CAMERA->getRelativeV2(shootEffectPos), _shootEffectAni);
 	}
 
 	// 재장전 중이라면 재장전 UI를 그린다.
