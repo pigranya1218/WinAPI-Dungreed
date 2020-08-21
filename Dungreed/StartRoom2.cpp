6ac9b96508ac59386def27395fdeb9174ff678ae
Hwang2442 <68269611+Hwang2442@users.noreply.github.com>
2020-08-21 14:44:04 +0900

    스타트스테이지 위치 수정

--- Dungreed/StartRoom2.cpp
+++ Dungreed/StartRoom2.cpp
@@ -7,8 +7,10 @@ void StartRoom2::init()
 	loadMap("room/StartRoom2_RB.map");
 
 	/*makeDoor(Vector2(TILESIZE * 24 + TILESIZE * 0.5, TILESIZE * 9), DIRECTION::RIGHT);
-	makeDoor(Vector2(TILESIZE * 19, TILESIZE * 14 + TILESIZE * 0.5), DIRECTION::DOWN);*/
-
+	makeDoor(Vector2(TILESIZE * 19, TILESIZE * 14 + TILESIZE * 0.5), DIRECTION::DOWN);*/	
+	_respawnPosition[0] = Vector2(300, 650); // 맨 처음 초기화되는 위치
+	_respawnPosition[2] = Vector2(1400, 650);
+	_respawnPosition[3] = Vector2(1150, 650); // 오른쪽에서 들어올 때 위치
 }
 
 void StartRoom2::release()
--- Dungreed/StartRoom3.cpp
+++ Dungreed/StartRoom3.cpp
@@ -6,8 +6,9 @@ void StartRoom3::init()
 	_tileImage = IMAGE_MANAGER->findImage("sampleTile1");
 	loadMap("room/StartRoom3_B.map");
 
-	// makeDoor(Vector2(TILESIZE * 24 + TILESIZE * 0.5, TILESIZE * 9), DIRECTION::DOWN);
-
+	// makeDoor(Vector2(TILESIZE * 24 + TILESIZE * 0.5, TILESIZE * 9), DIRECTION::DOWN);	
+	_respawnPosition[0] = Vector2(300, 650); // 맨 처음 초기화되는 위치
+	_respawnPosition[3] = Vector2(600, 650); // 오른쪽에서 들어올 때 위치
 }
 
 void StartRoom3::release()
