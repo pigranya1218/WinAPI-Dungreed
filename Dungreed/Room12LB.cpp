900fcb7bc438e700a49632d843a82ccdbbf0d31c
milrow321 <59471824+milrow321@users.noreply.github.com>
2020-08-21 13:45:27 +0900

    버그 수정, 맵 추가

---
+++ Dungreed/Room12LB.cpp
@@ -0,0 +1,23 @@
+#include "Room12LB.h"
+
+void Room12LB::init()
+{
+	Stage::init();
+	_tileImage = IMAGE_MANAGER->findImage("sampleTile1");
+	loadMap("room/Stage12_LB.map");
+}
+
+void Room12LB::release()
+{
+	Stage::release();
+}
+
+void Room12LB::update(float const elapsedTime)
+{
+	Stage::update(elapsedTime);
+}
+
+void Room12LB::render()
+{
+	Stage::render();
+}
