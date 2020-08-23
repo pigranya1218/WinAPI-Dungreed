676f7c7fa1af9906eb2b5216e211654545983f6e
DESKTOP-7D0JAE0\강상구 <tkdrn2485@naver.com>
2020-08-23 17:35:28 +0900

    단창 수정

--- Dungreed/ShortSpear.cpp
+++ Dungreed/ShortSpear.cpp
@@ -130,6 +130,18 @@ void ShortSpear::attack(Player* player)
 	CAMERA->pushShakeEvent(10, 0.1f);
 
 	bool isLeft = (player->getDirection() == DIRECTION::LEFT);
+	Vector2 pos = player->getPosition();
+	Vector2 renderPosHand = pos;
+	renderPosHand.x += ((isLeft) ? (_img->getWidth() * 0.15f * 4) : -(_img->getWidth() * 0.15f * 4)); // 손의 위치는 무기의 회전 중심점
+	renderPosHand.y += 20; // 플레이어의 중점으로부터 무기를 들고 있는 높이
+	// 손으로부터 마우스 에임까지의 각도
+	float angle = atan2f(-(CAMERA->getAbsoluteY(_ptMouse.y) - renderPosHand.y), (CAMERA->getAbsoluteX(_ptMouse.x) - renderPosHand.x)) + PI2;
+	if (angle > PI2)
+	{
+		angle -= PI2;
+	}
+	
+	_attackAngle = angle;
 		_reverseMove = false;
 		_currAttackDelay = _adjustStat.attackSpeed;
 	Vector2 originPos = player->getPosition();
