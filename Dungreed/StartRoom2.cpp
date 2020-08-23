72d1cb1a9a53fe83522111e757485143b0050263
kyungwoong kang <ddodd34@gmail.com>
2020-08-23 23:06:30 +0900

    스테이지 관상용 렌더링 예시 병합

--- Dungreed/StartRoom2.cpp
+++ Dungreed/StartRoom2.cpp
@@ -64,11 +64,31 @@ void StartRoom2::render()
 {
 	
 
-	Stage::render();
+	D2D_RENDERER->fillRectangle(FloatRect(0, 0, TILESIZE * 30, TILESIZE * 20), 51, 49, 67, 1);
+	for (int i = 0; i < _tile[0].tileX * _tile[0].tileY; ++i)
+	{
+		if (_tile[i].tileFrameX[0] != -1)
+		{
+			_tileImage->setScale(4);
+			CAMERA->frameRender(_tileImage, _tile[i].rc.getCenter(), _tile[i].tileFrameX[0], _tile[i].tileFrameY[0]);
+		}
+		if (_tile[i].tileFrameX[1] != -1)
+		{
+			_tileImage->setScale(4);
+			CAMERA->frameRender(_tileImage, _tile[i].rc.getCenter(), _tile[i].tileFrameX[1], _tile[i].tileFrameY[1]);
+		}
+	}
+
 	int sizeX = _torchImg->getSize().x * 4;
 	int sizeY = _torchImg->getSize().y * 4;
 	_torchImg->aniRender(CAMERA->getRelativeV2(Vector2(350, 380)), Vector2(sizeX, sizeY), _torchAni);
 	_torchImg->aniRender(CAMERA->getRelativeV2(Vector2(800, 380)), Vector2(sizeX, sizeY), _torchAni);
 	_torchImg->aniRender(CAMERA->getRelativeV2(Vector2(1060, 380)), Vector2(sizeX, sizeY), _torchAni);
+
+	_npcMgr->render();
+	_enemyMgr->render();
+	_objectMgr->render();
+	_projectileMgr->render();
+	
 }
 
